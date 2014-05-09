76c523a67b91d366d675701bf268df86dbb483b3
Brandon McGregor <iambrandonmcgregor@gmail.com>
2014-05-09 17:31:34 -0400

    Added scroll event handling when a 'theater' attribute is provided.

--- Invigorate.js
+++ Invigorate.js
@@ -24,15 +24,23 @@
   // -----------------------
 	Invigorate = function Invigorate (config) {
 
-    // setup the theater.
-    this.theater = config.theater;
+    // cache a reference to the current scope.
+    var self = this;
 
-    // setup the setpieces placeholder.
-    this.setPieces = config.setPieces;
+    // Apply all configuration parameters to this object.
+    for (var param in config) {
+      this[param] = config[param];
+    }
 
-    this.numberOfFrames = (config.numberOfFrames || 100);
+    // Ensure default configurations are provided where necessary.
+    if (!this.numberOfFrames) this.numberOfFrames = 100;
 
+    // Calculate the frame values.
     this.reinvigorate();
+
+    // Attach scroll listeners if a 'theater' element was provided.
+    if (this.theater)
+      this.theater.addEventListener('scroll', this.scrollHandler);
 
     return this;
   };
@@ -43,39 +51,65 @@
   	var self = this,
 		    workers = [];
 
-    for (var i=0, l=this.setPieces.length; i<l; i++) {
-      workers[i] = new Promise(function (resolve, reject) {
+    (function GenerateFrames () {
+      for (var i=0, l=self.setPieces.length; i<l; i++) {
+        workers[i] = new Promise(function (resolve, reject) {
 
-        // Calculate all frame values (fill in between keyframes).
-        Threadit(CreateFrames, {
-          numberOfFrames: self.numberOfFrames,
-          keyframes: self.setPieces[i].keyframes,
-          setPieceIdx: i
-        })
+          // Calculate all frame values (fill in between keyframes).
+          Threadit(CreateFrames, {
+            numberOfFrames: self.numberOfFrames,
+            keyframes: self.setPieces[i].keyframes,
+            setPieceIdx: i
+          })
 
-        // Turn frame values into css styles.
-        .then(function (framesObj) {
-          return Threadit(RawFramesToHtmlStyles, {
-            frames: framesObj.frames,
-            setPieceIdx: framesObj.setPieceIdx
+          // // Turn frame values into css styles.
+          // .then(function (framesObj) {
+
+          //   // Calculate CSS styles if an element is provided for the set-piece...
+          //   // ...in this case, we assume animating will be handled by Invigorate.
+          //   if (self.setPieces[framesObj.setPieceIdx].element) {
+          //     return Threadit(RawFramesToHtmlStyles, {
+          //       frames: framesObj.frames,
+          //       setPieceIdx: framesObj.setPieceIdx
+          //     });
+          //   }
+          //   else
+          //     return framesObj;
+          // })
+
+          // Attach the css-style frames to the setpieces.
+          .then(function (framesObj) {
+            self.setPieces[framesObj.setPieceIdx].frames = framesObj.frames;
+            resolve(framesObj.frames);
           });
-        })
 
-        // Attach the css-style frames to the setpieces.
-        .then(function (framesObj) {
-          self.setPieces[framesObj.setPieceIdx].frames = framesObj.frames;
-          resolve(framesObj.frames);
         });
+      }
+    })();
 
-      });
-    }
+    (function CacheScrollInfo () {
+      if (self.theater)
+        self.theater.maxScroll =
+          self.theater.scrollWidth - self.theater.clientWidth;
+    })();
 
+    // Resolve this promise when all of the frames have been created.
     this.promise = Promise.all(workers).then(function () {
     	return self.setPieces;
     });
 
     return this;
   };
+  Invigorate.prototype.scrollHandler = function scrollHandler(event) {
+
+    //TODO: Debounce this using rAF.
+
+    //TODO: Calculate and attach the 'currentFrame' variable.
+
+    console.log(event);
+    event.preventDefault();
+    return false;
+  }
 
   // Private Functions.
   // ------------------
@@ -100,6 +134,8 @@
       // attach properties on this keyframe.
       for (var pi=0, pl=properties.length; pi<pl; pi++) {
         property = properties[pi];
+
+        // TODO: Handle keyframe values set as strings with 'px' or '%'.
 
         // if property is set on this keyframe...
         if (keyframes[i] && typeof keyframes[i][property.name] !== 'undefined') {
@@ -178,9 +214,74 @@
   }
 
   function RawFramesToHtmlStyles (options) {
-  	console.log('RawFramesToHtmlStyles');
+
+    // setup some recycled variables.
+    var frame, propertyValue, style;
+
+    // loop through all the frames.
+    for (var i=0, l=options.frames.length; i<l; i++) {
+      frame = options.frames[i];
+
+      // reset the style string before we populate it.
+      style = '';
+
+      // loop through all the properties in this frame.
+      for (var property in frame) {
+
+        // convert the properties to be CSS formats.
+        frame[property] = GetCSSFriendlyProperty(property, frame[property]);
+
+        // append this property to the style attribute of this frame.
+        style += (property + ': ' + frame[property] + '; ');
+      }
+
+      // attach the style string to the frame.
+      frame.style = style.slice(0,-1);
+    }
+
+    // PRIVATE FUNCTION(S).
+    // --------------------
+    function GetCSSFriendlyProperty (property, value) {
+
+      switch (property) {
+        case 'height':
+        case 'width':
+          value = value + 'px';
+          break;
+        default:
+          break;
+      }
+
+      return value;
+    }
+
   	return options;
   }
 
   return Invigorate;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This space is intentionally left blank.
+
+
+
+
+
+
+
+
+
